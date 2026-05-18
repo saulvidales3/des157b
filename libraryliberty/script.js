c49cd0918e401da1d6b588504928c49e92aa9a3d
saul_vidales <saulvidales3@gmail.com>
2026-05-18 02:29:20 -0700

    p

--- libraryliberty/script.js
+++ libraryliberty/script.js
@@ -1,5 +1,22 @@
 (function() {
- 
+  var granimInstance = new Granim({
+      element: '#granim-canvas',
+      direction: 'diagonal',
+      isPausedWhenNotInView: true,
+      states: {
+        'default-state': {
+          gradients: [
+            ['#f7971e', '#ffd200'],
+            ['#f7971e', '#c0392b'],
+            ['#c0392b', '#8e44ad'],
+            ['#8e44ad', '#ffd200'],
+          ],
+          transitionSpeed: 6000
+        }
+      }
+    });
+
+    
 
    
 
